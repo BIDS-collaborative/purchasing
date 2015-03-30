57185e14d401b2073f653a057b804be0897c4528
juanshishido <juanshishido@cal.berkeley.edu>
2015-03-30 01:15:42 -0700

    Improved text preprocessing. Write topics and purchase descriptions to file.

--- script/purchasing_lda.py
+++ script/purchasing_lda.py
@@ -1,5 +1,4 @@
 import re
-import os.path
 import random
 import lda
 import csv
@@ -22,11 +21,13 @@ def main():
     cols = ['supplier_name', 'item_type', 'product_description', 'manufacturer',
             'buyer__first_name', 'buyer__last_name', 'department_name']
     for col in cols:
-        df[col] = df[col].replace(np.nan, '' , regex=True)                      \
-                         .apply(lambda x: x.lower())                            \
-                         .apply(lambda x: re.sub('[^A-Za-z0-9.%]+', ' ', x))    \
-                         .apply(lambda x: re.sub('^\.+', '', x))                \
-                         .apply(lambda x: re.sub('^\/', '', x))                 \
+        df[col] = df[col].replace(np.nan, '' , regex=True)                                      \
+                         .apply(lambda x: x.lower())                                            \
+                         .apply(lambda x: re.sub('(http\S*|www\S*)', '', x))                    \
+                         .apply(lambda x: re.sub('((?<=\D)/|/(?=\D))', ' ', x))                 \
+                         .apply(lambda x: re.sub('[^A-Za-z0-9.%\/]+', ' ', x))                  \
+                         .apply(lambda x: re.sub('\.+', '', x))                                 \
+                         .apply(lambda x: re.sub('(?<=\s)\w(?=\s)|(?<=\s)\d(?=\s)', '', x))     \
                          .apply(lambda x: re.sub('\s+', ' ', x).strip())
 
     # Product descriptions to list
@@ -34,7 +35,7 @@ def main():
     for i in xrange(0, df.product_description.size):
         pd_list.append(df.product_description[i])
 
-    # Initialize CountVevtorizer, removing stop words
+    # Initialize CountVevtorizer, remove stop words
     vectorizer = CountVectorizer(analyzer = "word",
                                  tokenizer = None,
                                  preprocessor = None,
@@ -47,23 +48,21 @@ def main():
     vocab = vectorizer.get_feature_names()
 
     # LDA
-    model = lda.LDA(n_topics=100, n_iter=1500, random_state=1)
+    model = lda.LDA(n_topics=100, n_iter=1500, random_state=8675309)
     model.fit(X)
 
-    # Print the words in each topic
+    # Output topic definitions to CSV (overwrites existing)
     topic_word = model.topic_word_
-    n_top_words = 8
-    print
-    for i, topic_dist in enumerate(topic_word):
-        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+    n_top_words = 11
+    with open('../../results/topic_definitions.csv', 'wb') as to_:
+        writer = csv.writer(to_, delimiter=',', quotechar='\"')
+        doc_topic = model.doc_topic_
+        for i, topic_dist in enumerate(topic_word):
+            topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+            writer.writerow([i, ' '.join(topic_words)])
 
-    # Output to CSV
-    if os.path.isfile('results/pd_topics.csv'):
-        method = 'a'
-    else:
-        method = 'wb'
-    with open('results/pd_topics.csv', method) as to_:
+    # Output purchase description designation to CSV (overwrites existing)
+    with open('../../results/pd_topics.csv', 'wb') as to_:
         writer = csv.writer(to_, delimiter=',', quotechar='\"')
         doc_topic = model.doc_topic_
         for i in range(len(pd_list)):
