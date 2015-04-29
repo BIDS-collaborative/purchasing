6cfe3379214acbef3fb4383943d7bfb30452e619
juanshishido <juanshishido@cal.berkeley.edu>
2015-04-29 00:34:05 -0700

    LDA ntbk and script: better cleaning & multi-topic option

--- script/purchasing_lda.py
+++ script/purchasing_lda.py
@@ -2,24 +2,31 @@ import re
 import random
 import lda
 import csv
-import pandas as pd
 import numpy as np
+import pandas as pd
 from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def main():
     # Load data
-    df = pd.read_csv('../../data/cleaned/UCB_dept_merge.csv')
+    df = pd.read_csv('../data/cleaned/UCB_dept_merge.csv')
 
-    # Randomly select 20k
+    # Concatenate supplier name and manufacturer
+    df['product_line'] = df['supplier_name'] + ' ' + \
+                         df['product_description'] + ' ' + \
+                         df['manufacturer']
+
+    # Randomly select 40k
     random.seed(8675309)
-    rows = random.sample(df.index, 20000)
+    rows = random.sample(df.index, 40000)
     df = df.ix[rows].reset_index()
 
     # Cleaning
     cols = ['supplier_name', 'item_type', 'product_description', 'manufacturer',
-            'buyer__first_name', 'buyer__last_name', 'department_name']
+            'buyer__first_name', 'buyer__last_name', 'department_name', 'product_line']
     for col in cols:
         df[col] = df[col].replace(np.nan, '' , regex=True)                                      \
                          .apply(lambda x: x.lower())                                            \
@@ -30,43 +37,66 @@ def main():
                          .apply(lambda x: re.sub('(?<=\s)\w(?=\s)|(?<=\s)\d(?=\s)', '', x))     \
                          .apply(lambda x: re.sub('\s+', ' ', x).strip())
 
-    # Product descriptions to list
-    pd_list = []
-    for i in xrange(0, df.product_description.size):
-        pd_list.append(df.product_description[i])
+    # Tokenize
+    tokenized_pd = [word_tokenize(line) for line in df.product_line]
 
-    # Initialize CountVevtorizer, remove stop words
-    vectorizer = CountVectorizer(analyzer = "word",
-                                 tokenizer = None,
-                                 preprocessor = None,
-                                 stop_words = 'english')
+    # Stop words
+    stop_words = stopwords.words('english') + \
+                 [u'ea', u'per', u'item', u'description', u'quote', u'pk', u'pack']
 
-    # Transform pd_list to a document-term matrix
-    X = vectorizer.fit_transform(pd_list).toarray()
+    # Remove stop words, numbers, single chars
+    tokenized_pd_clean = []
+    for entry in tokenized_pd:
+        entry_list = []
+        for word in entry:
+            if ((not word in stop_words) and (not unicode(word).isnumeric()) and (not len(word) <= 1)):
+                entry_list.append(word)
+        tokenized_pd_clean.append(entry_list)
+
+    # De-tokenize
+    pd_list_clean = []
+    for item in tokenized_pd_clean:
+        pd_list_clean.append(' '.join(item))
+
+    # Initialize CountVectorizer, remove stop words
+    vectorizer = CountVectorizer(analyzer = "word", 
+                                 tokenizer = None, 
+                                 preprocessor = None, 
+                                 stop_words = None)
+
+    # Transform pd_list_clean to a document-term matrix
+    X = vectorizer.fit_transform(pd_list_clean).toarray()
 
     # Unique words
     vocab = vectorizer.get_feature_names()
 
-    # LDA
-    model = lda.LDA(n_topics=100, n_iter=1500, random_state=8675309)
-    model.fit(X)
+    # Number of topics
+    for n_topics in range(5, 16):
 
-    # Output topic definitions to CSV (overwrites existing)
-    topic_word = model.topic_word_
-    n_top_words = 11
-    with open('../../results/topic_definitions.csv', 'wb') as to_:
-        writer = csv.writer(to_, delimiter=',', quotechar='\"')
-        doc_topic = model.doc_topic_
-        for i, topic_dist in enumerate(topic_word):
-            topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
-            writer.writerow([i, ' '.join(topic_words)])
+        # File names
+        t_def_fname = 'topics_definitions_' + str(n_topics) + '.csv'
+        t_fname = 'pd_topics_' + str(n_topics) + '.csv'
 
-    # Output purchase description designation to CSV (overwrites existing)
-    with open('../../results/pd_topics.csv', 'wb') as to_:
-        writer = csv.writer(to_, delimiter=',', quotechar='\"')
-        doc_topic = model.doc_topic_
-        for i in range(len(pd_list)):
-            writer.writerow([pd_list[i], doc_topic[i].argmax()])
+        # LDA
+        model = lda.LDA(n_topics=n_topics, n_iter=1500, random_state=8675309)
+        model.fit(X)
+
+        # Output topic definitions to CSV (overwrites existing)
+        topic_word = model.topic_word_
+        n_top_words = 21
+        with open('../results/'+t_def_fname, 'wb') as to_:
+            writer = csv.writer(to_, delimiter=',', quotechar='\"')
+            doc_topic = model.doc_topic_
+            for i, topic_dist in enumerate(topic_word):
+                topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+                writer.writerow([i, ' '.join(topic_words)])
+
+        # Output purchase description designation to CSV (overwrites existing)
+        with open('../results/'+t_fname, 'wb') as to_:
+            writer = csv.writer(to_, delimiter=',', quotechar='\"')
+            doc_topic = model.doc_topic_
+            for i in range(len(tokenized_pd_clean)):
+                writer.writerow([tokenized_pd_clean[i], doc_topic[i].argmax()])
 
 
 if __name__ == '__main__':
