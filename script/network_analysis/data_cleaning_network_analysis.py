5b5246287898d7d9b4915a437fe9d5b2b87eccbc
Chris Holdgraf <choldgraf@berkeley.edu>
2015-06-16 13:13:44 -0700

    Reorganizing a bunch of files and cleaning up some scripts

--- script/network_analysis/data_cleaning_network_analysis.py
+++ script/network_analysis/data_cleaning_network_analysis.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import numpy as np
-
-
-
 """
 Data cleaning
 The original dataset does not contain the proper department names, so
@@ -11,75 +6,59 @@ create a dictionary from
 the data and use it to assign department names to the original dataset with
 the network connections
 """
+
+import pandas as pd
+import numpy as np
+
 data = pd.read_csv('/Users/dariushbozorgmehri/Documents/My Work/Berkeley Classes Spring 2015/BIDS/data/modified_data_4_15/departmentDataV3_ALL_DATA_CLEAN.csv', low_memory=False)
 
-
-"""
-Rename the columns
-"""
+# Rename the columns for readability
 data = data.rename(columns={'Org.Level.4': 'org_level_4', 'Org.Level.4.Number': 'org_level_4_number','Org.Level.5': 'org_level_5', 'Org.Level.5.Number': 'org_level_5_number'})
 
 
-"""
-create data frame with org level 4 data only
-"""
+
+# create data frame with org level 4 data only
 data4Number = data[["org_level_4","org_level_4_number"]]
+
 #drop duplicates from the dataset
 data4Number = data4Number.drop_duplicates()
 data4Number = data4Number[["org_level_4_number", "org_level_4"]]
-#need to reindex after drop dup or cannot create a dictionary
+
+# need to reindex after drop dup or cannot create a dictionary
 data4Number.index = range(1,len(data4Number) + 1)
 
 
-"""
-do same as above for org leve 5 data
-"""
+
+# do same as above for org leve 5 data
 data5Number = data[["org_level_5","org_level_5_number"]]
 data5Number = data5Number.drop_duplicates()
 data5Number = data5Number[["org_level_5_number", "org_level_5"]]
 data5Number.index = range(1,len(data5Number) + 1)
 
-
-"""
-Create a dictionary for org elevel 4
-"""
-i=1
-for i in range(1,len(data4Number)):
-    #while i < 3:
+# Create a dictionary for org elevel 4
+for i in range(1, len(data4Number)):
     data4Dic.update({data4Number.org_level_4_number[i]:data4Number.org_level_4[i]}) 
-    
 
-"""
-create dic for org level 5 data
-"""
-i=1
+# create dic for org level 5 data
 for i in range(1,len(data5Number)):
-    #while i < 3:
     data5Dic.update({data5Number.org_level_5_number[i]:data5Number.org_level_5[i]})
 
 
-"""
-Create dataframe from cleaned dataset (dataset cleaned by Chris Holdgraf
-"""
+# Create dataframe from cleaned dataset (dataset cleaned by Chris Holdgraf)
 dataOrig = pd.read_csv('/Users/dariushbozorgmehri/Documents/My Work/Berkeley Classes Spring 2015/BIDS/data/UCB_dept_merge.csv', low_memory=False)
 
 
-"""
-The original department name contains the 5 digit code of the department
-and unspecific (general) department name. Split the orginal departname into
-department number and the unspecified dept name
-"""
+# The original department name contains the 5 digit code of the department
+# and unspecific (general) department name. Split the orginal departname into
+# department number and the unspecified dept name
 dataOrig['department_number'] = dataOrig["department_name"].apply(lambda x: x[:5])
 dataOrig['department_name_stripped'] = dataOrig["department_name"].apply(lambda x: x[6:])
 
 
-"""
-Use the dicitonaries to create a new department name in the dataOrig
-dataframe using the more accurate data
-from the above created dicitonaries, all of the most accurate department names
-are in the department_name_update variable
-"""
-i=0
+# Use the dicitonaries to create a new department name in the dataOrig
+# dataframe using the more accurate data
+# from the above created dicitonaries, all of the most accurate department names
+# are in the department_name_update variable
 for i in range(1,len(dataOrig)):
     if  dataOrig.department_number[i] in data4Dic:
         #print "in there"
--- script/network_analysis/network_analysis.py
+++ script/network_analysis/network_analysis.py
@@ -1,70 +1,31 @@
-
 # coding: utf-8
-
-# In[78]:
-
 import os
 import networkx as nx
 import matplotlib.pyplot as plt
 import cython
 import numpy as np
 import pandas as pd
-
-
-# In[59]:
+from networkx.algorithms import bipartite
 
 # load in merged data
 data_path = '../../../UCB_dept_merge.csv'
 mergeData = pd.read_csv(data_path)
 
-
-# In[60]:
-
 # cut out unnecessary info on department name
 mergeData["department_name"] = mergeData["department_name"].apply(lambda x: x.split()[0])
 
-
-# In[71]:
-
 # just keep department_name and supplier_name
 deptSupplier = mergeData[['department_name','supplier_name']]
-
-
-# In[97]:
-
-#keep only the unique values
 wanted = deptSupplier.drop_duplicates()
 
-
-# In[84]:
-
-#try to create graphs
-from networkx.algorithms import bipartite
-
-
-# In[98]:
-
-#make a bipartite graph
-test= nx.Graph()
+#make a bipartite graph, each edge is a co-occurrence of columns
+test = nx.Graph()
 test.add_nodes_from(wanted['department_name'], bipartite = 0)
 test.add_nodes_from(wanted['supplier_name'], bipartite = 1)
 edges = [tuple(x) for x in wanted.values]
 test.add_edges_from(edges)
 
-
-# In[ ]:
-
+# Graph visualization
 nx.draw(test)
 plt.show()
 plt.savefig("test.png")
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
